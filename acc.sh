fca3e1026a768eb6af3eec8029611f6033dc8246
Eddie Antonio Santos @ Calzone <easantos@ualberta.ca>
2017-03-06 08:54:17 -0700

    Add fancy command line parsing to acc script.

--- acc.sh
+++ acc.sh
@@ -8,13 +8,98 @@ EPOCH=1
 ACC=2
 VAL_ACC=4
 
-location="${2:-$(realpath ~easantos/training-grammar-guru/models)}"
-csvfile="$location/training.log"
-device="${1:-dumb}"
+SCRIPT_NAME="${0##*/}"
 
-if [ -s "$csvfile" ] ; then
-    csvfile="$(realpath "$csvfile")"
-else
+# Adapated from:
+# http://mywiki.wooledge.org/BashFAQ/035#CA-485294e4ff594e7d9e7c9c09b4d4110339260cf7_1
+show_help() {
+    cat <<EOF
+Usage:
+    ${SCRIPT_NAME} [--model=MODEL] [--display=DISPLAY|--qt] [PATH_TO_MODEL_DIR]
+    ${SCRIPT_NAME} [--list]
+
+Displays status on the accuaracy of the models or lists the models.
+
+    --help      show this screen and exit
+    --list      lists the existing models
+    --model     specify a model using N1,N2,...,Nn notation
+    --display   passed directly into gnuplot's terminal argument
+EOF
+}
+
+show_list() {
+    ls -d ~easantos/Backups/models/*/
+}
+
+device="dumb"
+model=
+
+while :; do
+    case $1 in
+        -h|--help)   # Call a "show_help" function to display a synopsis, then exit.
+            show_help
+            exit
+            ;;
+        -l|--list)   # Call a "show_help" function to display a synopsis, then exit.
+            show_list
+            exit
+            ;;
+        -m|--model)       # Takes an option argument, ensuring it has been specified.
+            if [ -n "$2" ]; then
+                model=$2
+                shift
+            else
+                printf 'ERROR: "--model" requires a non-empty option argument.\n' >&2
+                exit 1
+            fi
+            ;;
+        --model=?*)
+            model=${1#*=} # Delete everything up to "=" and assign the remainder.
+            ;;
+        --model=)         # Handle the case of an empty --model=
+            printf 'ERROR: "--model" requires a non-empty option argument.\n' >&2
+            exit 1
+            ;;
+        -d|--device)       # Takes an option argument, ensuring it has been specified.
+            if [ -n "$2" ]; then
+                device="$2"
+                shift
+            else
+                printf 'ERROR: "--device" requires a non-empty option argument.\n' >&2
+                exit 1
+            fi
+            ;;
+        --device=?*)
+            device=${1#*=} # Delete everything up to "=" and assign the remainder.
+            ;;
+        --device=)         # Handle the case of an empty --model=
+            printf 'ERROR: "--device" requires a non-empty option argument.\n' >&2
+            exit 1
+            ;;
+        --qt)
+            device=qt
+            ;;
+        --)              # End of all options.
+            shift
+            break
+            ;;
+        -?*)
+            printf 'WARN: Unknown option (ignored): %s\n' "$1" >&2
+            ;;
+        *)               # Default case: If no more options then break out of the loop.
+            if [ -n "$1" ] ; then
+                location="$1"
+            fi
+            break
+            ;;
+    esac
+    shift
+done
+
+location="${location:-$(realpath ~easantos/training-grammar-guru/models)}"
+csvfile="$location/training.log"
+
+if [ ! -s "$csvfile" ] ; then
     echo "Not a valid training log: $csvfile" 1>&2
     exit -1
 fi
